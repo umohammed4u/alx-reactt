9d9f9670a4ea077ea032e7e7132825c299a55c48
Olumide Micheal <olumidemicheal7@gmail.com>
2023-01-26 19:00:15 +0100

    can't figure it out yet

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from "react-dom/client";
 import App from './App';
 import Notifications from './Notifications';
-import reportWebVitals from "./reportWebVitals";
 
 
 const rootElement = document.getElementById("root");
@@ -14,5 +13,3 @@ root.render(
 		<App />
 	</React.StrictMode>
 );
-
-reportWebVitals();
