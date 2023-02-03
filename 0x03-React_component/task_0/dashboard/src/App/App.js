b5156df3e75c5574808117753cf6f61c02cef41a
Olumide Micheal <olumidemicheal7@gmail.com>
2023-02-03 14:16:54 +0100

    fixed state

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -10,25 +10,26 @@ import { getLatestNotification } from '../utils/utils';
 
 
 class App extends React.Component {
-  listCourses = [
+  state = {
+    listCourses: [
       {id: 1, name: 'ES6', credit: 60},
       {id: 2, name: 'Webpack', credit: 20},
       {id: 3, name: 'React', credit:40}
-    ]
-  
-    listNotifications = [
+    ],
+    listNotifications: [
       {id: 1, type: 'default', value: 'New course available'},
       {id: 2, type: 'urgent', value: 'New resume available'},
       {id: 3, type: 'urgent', html: getLatestNotification()}
     ]
-
+  }
+  
   render() {
     return (
       <React.Fragment>
-        <Notifications listNotifications={this.listNotifications}/>
+        <Notifications listNotifications={this.state.listNotifications}/>
         <div className="App">
           <Header />
-          {this.props.isLoggedIn ? <CourseList listCourses={this.listCourses}/> : <Login />}
+          {this.props.isLoggedIn ? <CourseList listCourses={this.state.listCourses}/> : <Login />}
           <Footer />
         </div>
       </React.Fragment>
