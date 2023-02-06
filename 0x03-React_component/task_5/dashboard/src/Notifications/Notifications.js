ac544b69398b6c1c8e8557f3e2aa0d0d4072c039
Olumide Micheal <olumidemicheal7@gmail.com>
2023-02-06 16:22:03 +0100

    Make your own pure component

--- 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.js
+++ 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.js
@@ -16,6 +16,10 @@ class Notifications extends React.Component {
     console.log(`Notification ${id} has been marked as read`);
   }
 
+  shouldComponentUpdate(nextProps) {
+    return nextProps.listNotifications.length > this.props.listNotifications.length;
+  }
+
   render() {
     return (
       <div className='wrapper'>
