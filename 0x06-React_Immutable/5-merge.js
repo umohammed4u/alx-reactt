c76abb49e904208e1895c11968b049a9afb2ddec
Olumide Micheal <olumidemicheal7@gmail.com>
2023-02-16 20:11:35 +0100

    Merge & concat

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,18 +1,18 @@
 import { List } from 'immutable';
 
-function concatElements(page1, page2) {
-    return List(page1).concat(List(page2));
+function concatElements (page1, page2) {
+  return List(page1).concat(List(page2));
 }
 
-function mergeElements(page1, page2) {
-    if(page1 === page2) {
-        return List(page2);
-    } else {
-        return List(page1).concat(List(page2));
-    }
+function mergeElements (page1, page2) {
+  if (page1 === page2) {
+    return List(page2);
+  } else {
+    return List(page1).concat(List(page2));
+  }
 }
 
 export {
-    concatElements,
-    mergeElements
-}
+  concatElements,
+  mergeElements
+};
